b72af1878f6ecabe888de34b9bba923c2ce9ab30
sebagruia <blackthorus@gmail.com>
2025-02-13 19:55:19 +0200

    Updated manifest images path

--- vite.config.js
+++ vite.config.js
@@ -23,18 +23,18 @@ export default defineConfig({
         orientation: 'any',
         icons: [
           {
-            src: '/favicon.ico',
+            src: '/TheRightWay//favicon.ico',
             sizes: '64x64 32x32 24x24 16x16',
             type: 'image/x-icon',
           },
           {
-            src: '/TheRightWaylogo512.png',
+            src: '/TheRightWay//TheRightWaylogo512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
           },
           {
-            src: '/TheRightWaylogo192.png',
+            src: '/TheRightWay//TheRightWaylogo192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any',
@@ -42,7 +42,7 @@ export default defineConfig({
         ],
         screenshots: [
           {
-            src: '/TheRightWayscreenshot.png',
+            src: '/TheRightWay//TheRightWayscreenshot.png',
             sizes: '2560x1431',
             type: 'image/png',
             description: 'A screenshot of the home page',
