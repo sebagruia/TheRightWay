cd6cd5200adaa285ff1d771a6efe43783d377860
sebagruia <blackthorus@gmail.com>
2025-02-18 19:33:32 +0200

    Updated CI git action

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -24,13 +24,13 @@ import { ModalHeaderBackground } from '../interfaces/modal';
 import { fetchListItemsAction, fetchUserListAction } from '../redux/list/listActions';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB6b4D40cLSubf_qDK7BzKMDnoH_l_2N1A',
-  authDomain: 'therightway-e6f15.firebaseapp.com',
-  projectId: 'therightway-e6f15',
-  storageBucket: 'therightway-e6f15.appspot.com',
-  messagingSenderId: '590591734476',
-  appId: '1:590591734476:web:36a7e413a992f663df4869',
-  measurementId: 'G-G7G582CTJS',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN_FIREBASE,
+  projectId: import.meta.env.VITE_PROJECT_ID_FIREBASE,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID_FIREBASE,
+  appId: import.meta.env.VITE_APP_ID_FIREBASE,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID_FIREBASE,
 };
 
 firebase.initializeApp(firebaseConfig);
